f81e98ec4ad24e08423b5dc5077ecd5e28d67b50
Anand Chowdhary <anandchowdhary@gmail.com>
2018-09-17 11:33:45 +0200

    Add link generator

---
+++ src/components/LinkGenerator.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <h1>Calendar Event Link</h1>
+    <p>Enter your event information below for a custom invitation link you can share with your guests.</p>
+    <form>
+      <label>
+        <span>Title</span>
+        <input type="text" placeholder="Coffee with Anand">
+      </label>
+      <label>
+        <span>Title</span>
+        <input type="text" placeholder="Coffee with Anand">
+      </label>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LinkGenerator"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.hello {
+  max-width: 500px;
+  margin: 10vh auto;
+}
+form {
+  text-align: left;
+  label {
+    display: block;
+    span {
+      font-weight: bold;
+    }
+    + label {
+      margin-top: 1.5rem;
+    }
+  }
+}
+input,
+button,
+textarea,
+select {
+  display: block;
+  width: 100%;
+  box-sizing: border-box;
+  font: inherit;
+  padding: 1rem;
+  margin-top: 0.35rem;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LinkGenerator />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import LinkGenerator from "@/components/LinkGenerator.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    LinkGenerator
   }
 };
 </script>
