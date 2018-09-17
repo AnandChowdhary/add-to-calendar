cb0086cfb20c385da21352ac0d8cff828ac8c8ea
Anand Chowdhary <anandchowdhary@gmail.com>
2018-09-17 11:56:52 +0200

    Add invite page

--- src/router.js
+++ src/router.js
@@ -10,15 +10,12 @@ export default new Router({
       path: "/",
       name: "home",
       component: Home
+    },
+    {
+      path: "/:title/:date/:time",
+      name: "invite",
+      component: () =>
+        import(/* webpackChunkName: "invite" */ "./views/Invite.vue")
     }
-    // {
-    //   path: "/about",
-    //   name: "about",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () =>
-    //     import(/* webpackChunkName: "about" */ "./views/About.vue")
-    // }
   ]
 });
---
+++ src/views/Invite.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="invite">
+	<div>title: {{title}}</div>
+	<div>date: {{date}}</div>
+	<div>time: {{time}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      title: "",
+      date: "",
+      time: ""
+    };
+  },
+  methods: {
+    unurize(x) {
+      return decodeURIComponent(atob(x));
+    }
+  },
+  mounted() {
+    this.title = this.unurize(this.$route.params.title);
+    this.date = this.unurize(this.$route.params.date);
+    this.time = this.unurize(this.$route.params.time);
+  }
+};
+</script>
